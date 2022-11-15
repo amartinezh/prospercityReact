0d813037a3cbb8fb0ccd960f1fd0f244e9ce7e1f
AlexisCasTawil <alexis_a_k@hotmail.com>
2022-11-15 15:32:42 -0500

    Changes from Alpha member, Alexis

---
+++ src/components/alpha/Alpha.js
@@ -0,0 +1,18 @@
+// import logo from './prosper.png';
+import '../../App.css';
+
+function Alpha() {
+  return (
+    <div className="Alpha">
+      <footer className="Alpha-footer"><strong>Alpha Team!</strong>
+      <br></br>
+        <span className="Alpha-member">
+          Alexis C.
+        </span>
+        
+      </footer>
+    </div>
+  );
+}
+
+export default Alpha;
