e9674774b88da7d387a97db19586a4cfd19ca72f
LuisGT <luisgomeztrujillo@gmail.com>
2022-11-17 23:51:24 -0500

    Agregados los integrantes de JumpForce

--- src/index.js
+++ src/index.js
@@ -11,6 +11,48 @@ import Alpha from './components/alpha/Alpha';
 import MayusJeison from './components/mayusjeison/MayusJeison';
 import { ButtonAlpha } from './components/alpha/ButtonAlpha';
 import { ConvertirAMayuscula } from './components/ConvertirAMayuscula/ConvertirAMayuscula';
+import {JumpForce} from './components/JumpForce/JumpForce';
+import {TeamList} from './components/TeamList/TeamList';
+import {TeamItem} from './components/TeamItem/TeamItem';
+
+const TeamMembers=[
+  {
+    memberName: 'Jeison Stiven Lopez Castro',
+    gitHubUrl: 'https://github.com/Jeisonlopez259911',
+    photoUrl: 'https://drive.google.com/uc?id=1OdCgof7erO9UnBMugD8oE1o_OLJyMx9p'
+  }, 
+  {
+    memberName: 'Santiago Londoño Salgado',
+    gitHubUrl: 'https://github.com/Santy149',
+    photoUrl: 'https://drive.google.com/uc?id=1Aab9RCtUiQ_kT8Q8z4Lb79mnVtizkCCH'
+  }, 
+  {
+    memberName: 'Carlos Alberto Orozco Ospina',
+    gitHubUrl: 'https://github.com/charlie1978',
+    photoUrl: 'https://avatars.githubusercontent.com/u/41816057?v=4'
+  },
+  {
+    memberName: 'Juan José Cardona Ramírez',
+    gitHubUrl: 'https://github.com/juanirmao',
+    photoUrl: 'https://drive.google.com/uc?id=1hE1qK9BOn-4vcJvI1bZlCZh-1YpNYe-d' 
+  }, 
+  {
+    memberName: 'Jhon Alexander Parra Calderon',
+    gitHubUrl: 'https://github.com/AlexparraC',
+    photoUrl: 'https://drive.google.com/uc?id=1fiKAslCLtRuGeXIXnox2T2tw4f38wLNw'
+  },
+  {
+    memberName: 'Jeremy Alejandro Ramirez Palacio',
+    gitHubUrl: 'https://github.com/Jexpalacio11',
+    photoUrl: 'https://drive.google.com/uc?id=1XO6TztH086V6aJ3nc4Gdr89UPH-Z5g4c'
+  },
+  {
+    memberName: 'Luis Alfonso Gómez Trujillo',
+    gitHubUrl: 'https://github.com/LuisGomezTrujillo',
+    photoUrl: 'https://drive.google.com/uc?id=1ngDLpH7hLFH8dU9jI9BtPlC4cQd4H_Ze'  
+  },  
+
+];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -21,7 +63,15 @@ root.render(
     <Mi />
     <Calcula />
     <Alpha />
-    <ButtonAlpha />     
+    <ButtonAlpha />   
+    <JumpForce />  
+    <TeamList>
+          {
+            TeamMembers.map(TeamMember=>(
+              <TeamItem key={TeamMember.memberName} memberName={TeamMember.memberName} gitHubUrl={TeamMember.gitHubUrl} photoUrl={TeamMember.photoUrl}/>
+            )
+          )};          
+    </TeamList>
     <MayusJeison/>
     <ConvertirAMayuscula />
   </React.StrictMode>
